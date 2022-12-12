cccaa804f060907b3b8705b0bddee0fbfa544cac
User <komplektmed@gmail.com>
2022-12-12 02:36:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello Ukraine!\nHallo Hallo");
+        System.out.print("Hallo");
+        System.out.print("world\n");
+        System.out.println(1000);
+
+        /*
+        Домашнее задание 08.12.2022
+        Задание №3: В методе main
+        инициализировать все примитивные типы
+        и вывести их в консоль
+         */
+
+        byte    a = 3;
+        short   b = 70;
+        int     c = 210;
+        long    d = 1_000_000;
+        float  pi = 3.14F;
+        double  p = 19.10;
+        boolean e = true;
+        boolean z = false;
+        char    t = 'W';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(pi);
+        System.out.println(p);
+        System.out.println(e);
+        System.out.println(z);
+        System.out.println(t);
+
+    }
+}
