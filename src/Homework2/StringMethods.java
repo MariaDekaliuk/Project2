9f8723e36c5f2b2c9ac369126e8ab34c571bded5
User <komplektmed@gmail.com>
2023-01-09 01:28:05 +0100

    ß=)

--- src/Homework2/StringMethods.java
+++ src/Homework2/StringMethods.java
@@ -1,6 +1,5 @@
 package Homework2;
 
 public class StringMethods {
-    public static String staticText = null; //объявили статичную переменную типа String
-    public String variableText;
+        public String variableText;
 }
