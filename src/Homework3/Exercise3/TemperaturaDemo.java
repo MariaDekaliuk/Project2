98beed417a8a692b1eb4894a9e08a63ddba2cec3
User <komplektmed@gmail.com>
2023-01-15 01:12:44 +0100

    ** еще в процессе...

--- src/Homework3/Exercise3/TemperaturaDemo.java
+++ src/Homework3/Exercise3/TemperaturaDemo.java
@@ -7,7 +7,7 @@ public class TemperaturaDemo {
             int temperatur1za100 = Request.intRequest("Укажите температуру колбы 1 (int)");
             int temperatur2do100 = Request.intRequest("Укажите температуру колбы 2 (int)");
 
-            System.out.println("Устройство работает нормально? - " + Temperatura.check(temperatur1za100, temperatur2do100));
-
+            System.out.println("Устройство работает нормально? - "
+                    + Temperatura.check(temperatur1za100, temperatur2do100));
         }
 }
